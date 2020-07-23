45d7c99e7a105126cde231e21e93930612f6a776
cloudwhite <cloudwhite.bai@ftdigital.com.cn>
2020-07-23 14:25:46 +0800

    modify

---
+++ downLoadQBEbook.py
@@ -0,0 +1,190 @@
+#coding:utf-8
+import urllib
+import urllib.request
+import multiprocessing
+from bs4 import BeautifulSoup
+import re
+import os
+import time
+import unicodedata
+
+def get_pages(url):
+    soup=""
+    try:
+        # 创建请求日志文件夹
+        if 'Log' not in os.listdir('.'):
+            os.mkdir(r"./Log")
+        # headers = {
+        #     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
+        #     'Host':'www.23txt.com',
+        #     'Cookie': 'PPad_id_PP=1; hitme=2; hitbookid=37532',
+        #     'Connection': 'keep-alive',
+        #     'Accept': 'ext/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        #     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        #     'Accept-Encoding': 'gzip',
+        #     'Referer' : "https://www.23txt.com/files/article/html/37/37532/",
+        #     'content-type':'text/html; charset=gbk',
+        #     'transfer-encoding':'chunked',
+        #     'date': 'Thu, 23 Jul 2020 12:29:48 GMT',
+        #     'l-proxy-cache':'MISS',
+        #     'server':'nginx/1.16.1',
+        #     'vary':'Accept-Encoding',
+        #     'x-drive':'default',
+        #     'x-proxy-cache':'MISS'
+        # }
+        #params = urllib.parse.urlencode(data).encode(encoding='gbk')
+
+        # 请求当前章节页面  params为请求参数
+        #request = urllib.request.Request(url,headers)
+        request = urllib.request.Request(url)
+        response = urllib.request.urlopen(request)
+        content = response.read()
+        data = content.decode('gbk')
+        # soup转换
+        soup = BeautifulSoup(data, "html.parser")
+
+    except Exception as e:
+        print(e)
+        print(url+" 请求错误\n")
+        with open(r"./Log/req_error.txt",'a',encoding='utf-8') as f:
+            f.write(url+" 请求错误\n")
+        f.close()
+    return soup
+
+# 通过章节的url下载内容，并返回下一页的url
+def get_ChartTxt(url,title,num):
+    soup=get_pages(url)
+
+    # 获取章节名称
+    subtitle = soup.select('.bookname h1')[0].text
+    # 判断是否有感言
+    if re.search(r'.*?章', subtitle) is  None:
+        return
+    # 获取章节文本
+    content = soup.select('#content')[0].text
+    # 消除 \xa0
+    content = unicodedata.normalize('NFKC', content)
+    # 按照指定格式替换章节内容，运用正则表达式
+    content = re.sub(r'<br\s*?>', '\n', content)
+    content = re.sub(r'\(.*?\)', '', content)
+    content = re.sub(r'\r\n', '', content)
+    content = re.sub(r'\n+', '\n', content)
+    content = re.sub(r'<.*?>+', '\n', content)
+    content = re.sub(r'  ', '\n', content)
+
+
+    # 单独写入这一章
+    try:
+        with open(r'./%s/%s %s.txt' % (title, num,subtitle), 'w', encoding='utf-8') as f:
+            f.write(subtitle + content)
+        f.close()
+        print(num,subtitle, '下载成功')
+
+    except Exception as e:
+        print(subtitle, '下载失败',url)
+        errorPath='./Error/%s'%(title)
+        # 创建错误文件夹
+        try :
+            os.makedirs(errorPath)
+        except Exception as e:
+            pass
+        #写入错误文件
+        with open("%s/error_url.txt"%(errorPath),'a',encoding='utf-8') as f:
+            f.write(subtitle+"下载失败 "+url+'\n')
+        f.close()
+    return
+
+
+# 通过首页获得该小说的所有章节链接后下载这本书
+def thread_getOneBook(indexUrl):
+    base = 'https://www.23txt.com'
+    soup = get_pages(indexUrl)
+    # 获取书名
+    #title = soup.select('#htmldhshuming')[0].text
+    title = soup.select("#info h1")[0].text
+    # 根据书名创建文件夹
+    if title not in os.listdir('.'):
+        os.mkdir(r"./%s" % (title))
+        print(title, "文件夹创建成功———————————————————")
+
+    # 加载此进程开始的时间
+    print('下载 %s 的PID：%s...' % (title, os.getpid()))
+    start = time.time()
+
+    # 获取这本书的所有章节
+    charts_url = []
+    # 提取出书的每章节不变的url
+    indexUrl = re.sub(r'index.html', '', indexUrl)
+    print(indexUrl)
+    charts = soup.select("#list dd a")
+    for i in charts:
+        # print(i+i.attrs['href'])
+        charts_url.append(base + i.attrs['href'])
+
+    # 创建下载这本书的进程
+    p = multiprocessing.Pool()
+    #自己在下载的文件前加上编号，防止有的文章有上，中，下三卷导致有3个第一章
+    num=1
+    for i in charts_url:
+        p.apply_async(get_ChartTxt, args=(i,title,num))
+        num+=1
+    print('等待 %s 所有的章节被加载......' % (title))
+    p.close()
+    p.join()
+    end = time.time()
+    print('下载 %s  完成，运行时间  %0.2f s.' % (title, (end - start)))
+    print('开始生成 %s ................' %title )
+    sort_allCharts(r'./%s'%title,"%s.txt"%title)
+    return
+
+# 创建下载多本书书的进程
+def process_getAllBook(base):
+    # 输入你要下载的书的首页地址
+    print('主程序的PID：%s' % os.getpid())
+    book_indexUrl=[
+        'https://www.23txt.com/files/article/html/56/56263/',
+        'https://www.23txt.com/files/article/html/55/55651/',
+    ]
+    print("-------------------开始下载-------------------")
+    p = []
+    for i in book_indexUrl:
+        p.append(multiprocessing.Process(target=thread_getOneBook, args=(i,)))
+    print("等待所有的主进程加载完成........")
+    for i in p:
+        i.start()
+    for i in p:
+        i.join()
+    print("-------------------全部下载完成-------------------")
+
+    return
+
+#合成一本书
+def sort_allCharts(path,filename):
+    lists=os.listdir(path)
+    # 对文件排序
+    # lists=sorted(lists,key=lambda i:int(re.match(r'(\d+)',i).group()))
+    lists.sort(key=lambda i:int(re.match(r'(\d+)',i).group()))
+    # 删除旧的书
+    if os.path.exists(filename):
+        os.remove(filename)
+        print('旧的 %s 已经被删除'%filename)
+    # 创建新书
+    with open(r'./%s'%(filename),'a',encoding='utf-8') as f:
+        for i in lists:
+            with open(r'%s/%s' % (path, i), 'r', encoding='utf-8') as temp:
+                f.writelines(temp.readlines())
+            temp.close()
+    f.close()
+    print('新的 %s 已经被创建在当前目录 %s '%(filename,os.path.abspath(filename)))
+    return 1
+
+
+if __name__=="__main__":
+    # # 主页
+    #base = 'http://www.yznnw.com'
+    base = 'https://www.23txt.com'
+    # 下载指定的书
+    process_getAllBook(base)
+    #sort_allCharts(r'./我师兄实在太稳健了',"我师兄实在太稳健了.txt")
+    # sort_allCharts(r'./武道天下',"武道天下.txt")
+    # sort_allCharts(r'./修罗武神',"修罗武神.txt")
